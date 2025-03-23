55dcc29ea4f9809341db4d932cf3857fc885a817
Jose Franco <jofranco@my365.bellevue.edu>
2025-03-23 18:22:52 -0500

    Push 03/26/25

--- RanCardGen/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.rancardgen {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.rancardgen to javafx.fxml;
-    exports com.example.rancardgen;
-}
